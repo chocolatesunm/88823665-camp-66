9f44b1e445e13a80c5700a1a69e987ab6a7ae1dd
Natchaya <chocolatesunm@gmail.com>
2024-12-23 13:33:18 +0700

    php2

--- index.php
+++ index.php
@@ -1,66 +1,24 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title>PHP test</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css&quot; rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js&quot; integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </head>
     <body>
-        <h1>File index.php</h1>
-        <?php
-        echo "hello world";
-        echo "<br>";
-        print("hello");
-        echo "<br>";
-        print_r("hello");
-        echo "<br>";
-        print("hello");
-        echo "<br>";
-        var_dump("hello");
-        echo "<br>";
-        $myvar = "hello world";
-        ?>
-        <h1><?php echo $myvar; ?></h1>
-        <?php
-        echo "<h1>" .$myvar."</h1>";
-        ?>
-        <?php 
-        $x = 1;
-        function myfunction($myparam) {
-            global $x;
-            $x = "Hello";
-            return $myparam;
-        }
-        echo "<p>" .MYFUNCTION("hello world!"). "</p>";
-        ?>
-        <h1> <?php echo $x; ?> </h1>
-        <?php echo 1 + 1; ?>
-        <?php echo "1" + '1'; ?>
-        <?php 
-        $mychar = "a";
-        ?>
-        <h1> <?php echo ++$mychar; ?> </h1>
-    <?php
-    if(1 === '1'){
-        echo "1 == \"1\" ";
-    }else if(true){
-        echo "1 != \"1\"";
-    }
-    if(true)
-        echo  "true";
-        else
-        if(true)
-        echo "false";
-    $my_arry = array(1,2,3,4,5);
-    for($i=0;$i < count ($my_arry);$i++){
-        echo @-$my_arry[$i];
-    }
-    foreach ($my_arry as $index => $value){
-        echo $value;
-    }
-    $my_arry2 [] = 1;
-    $my_arry2 [2] = 2;
-    $my_arry2 []= 3;
-    print_r($my_arry2);
-        ?>
+        <div class="container mt-5">
+            <form method="post" action="">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                    <input name="email" type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                    <textarea name="detail" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <div class="mb-3">
+                    <button class="btn btn-success" type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
     </body>
 </html>
