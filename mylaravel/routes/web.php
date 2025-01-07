aaef14eea5f246a1b3954bc888a404dd918b5f2d
Natchaya <chocolatesunm@gmail.com>
2025-01-07 19:43:05 +0700

    controller

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('/hello', function () {
     return "<h1>Hello, World!</h1>";
 });
+/*route ไว้สำหรับเปลี่ยนหน้าจอ*/
+Route::get("/mycontroller/{id?}", [MyController::class, 'myfunction']);
