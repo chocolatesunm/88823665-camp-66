3ae1c37196d323e88c19986e435c4a1ad5a57839
Natchaya <chocolatesunm@gmail.com>
2025-01-14 18:57:02 +0700

    สูตรคูณ

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -3,12 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/hello', function () {
-    return "<h1>Hello, World!</h1>";
-});
-/*route ไว้สำหรับเปลี่ยนหน้าจอ*/
-Route::get("/mycontroller/{id?}", [MyController::class, 'myfunction']);
+Route::get('/mycontroller', [MyController::class, 'myfunction']);
+Route::post('/mycontroller', [MyController::class, 'myfunction']);
